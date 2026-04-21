5a28904bc6f7c18080d997282727d57102427ead
Jairo <ramosj06jairo@gmail.com>
2026-04-21 18:18:18 +0200

    agrego el layoust master

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PuraSonrisa</title>
+
+    <!-- Fuente de Google Fonts -->
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Playwrite+NO:wght@100..400&display=swap');
+    body { font-family: "Open Sans", sans-serif; }
+        h1, h2, h3 { font-family: "Playwrite NO", sans-serif; }
+    </style>
+
+    <!-- Icono de la pestaña -->
+     <link rel="icon" href="{{ asset('images/LogoPuraSonrisa.webp') }}" type="image/png">
+
+    <!-- Esto es para incluir los archivos de Vite y equivale al enlace de tailwind -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+<body>
+  
+</body>
+</html>
